51d7d1901e1a0366211c8a7840d20ddf8f231ce1
aMorr78proj <234978669+aMorr78proj@users.noreply.github.com>
2026-01-13 15:14:47 -0800

    Initial page setup, with slight styling for the boxes, and basic functionality to make boxes appear

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+
+function loadUp(num){
+
+    // checking that user input is reasonable
+    if (num > 100) { num = 100; }
+    else if (num < 0) { num = 0; }
+
+    // grabbing parent container for everything else later
+    let container = document.querySelector(".container");
+
+    // check for blocks to already be created
+    let prev = document.querySelectorAll(".boxes");
+
+    // removing previous boxes
+    if (prev.length > 0) {
+        for (i=0; i<prev.length; i++) {
+            container.removeChild(prev[i]);
+        }
+    }
+
+    // creating new set of boxes
+    for (i=0; i<num; i++) {
+        let blocks = document.createElement("div");
+        blocks.setAttribute("id", `block-${i}`);
+        blocks.classList.add("boxes");
+        container.appendChild(blocks);
+    }
+}
+
+// once document is loaded, add event listener to user input
+document.addEventListener('DOMContentLoaded', function() {
+    let input = document.getElementById("grid-no");
+    input.addEventListener("keypress", function(e) {
+        if (e.key === "Enter") {
+            loadUp(this.value);
+        }
+    });
+});
